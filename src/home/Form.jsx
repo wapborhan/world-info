cdf2af0612b628811642865ca5e4a333d573165b
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-11-11 23:01:20 +0600

    Update Form.jsx

--- src/home/Form.jsx
+++ src/home/Form.jsx
@@ -5,11 +5,15 @@ function InputForm() {
   return (
     <Form>
       <Form.Group
-        className="mb-3 d-flex justify-cotent-between"
+        className="mb-3 d-flex justify-content-center"
         controlId="formBasicEmail"
       >
-        <Form.Control type="text" placeholder="Enter Your Country Name" />
-        <Button variant="primary" type="submit">
+        <Form.Control
+          type="text"
+          className="w-50"
+          placeholder="Enter Your Country Name"
+        />
+        <Button variant="primary" className="w-5 mx-3" type="submit">
           Submit
         </Button>
       </Form.Group>
