c57539be44ca2ea8b687180c1970b8a1aed6a4da
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-11-16 19:54:55 +0600

    Dark Mood On

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+  const [isDark, setDark] = useState(false);
+  const toggleDark = () => {
+    setDark(!isDark);
+    // const html = document.getElementsByTagName("body");
+    // console.log(html);
+    document.body.classList.toggle("dark");
+  };
   return (
     <Navbar className="shadow header fixed-top" expand="lg">
       <Container>
@@ -14,7 +22,7 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">
+            <Nav.Link onClick={toggleDark}>
               <span>i</span>Dark Mode
             </Nav.Link>
           </Nav>
